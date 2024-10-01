bb5dbd66fb99ceb1e69af6f45942e34bb15f505e
Lautaro <lautaromartinvillalba@gmail.com>
2024-09-30 22:58:30 -0300

    CREATE PROJECT: with dependencies -> FlyWay, SpringWeb/JPA/Email/Web/Security, PostgreSQL, Lombok, Redis

---
+++ src/main/java/ar/com/l_airline/LAirlineApplication.java
@@ -0,0 +1,13 @@
+package ar.com.l_airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LAirlineApplication.class, args);
+	}
+
+}
