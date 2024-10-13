2e4f937556093bddb43e161237b28103d48251fb
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2024-10-12 23:00:26 -0300

    REFACTOR: change @PathVariable to @RequestParam in EndPoints variables in HotelController

--- src/main/java/ar/com/l_airline/controllers/HotelController.java
+++ src/main/java/ar/com/l_airline/controllers/HotelController.java
@@ -5,7 +5,6 @@ import ar.com.l_airline.entities.hotel.HotelDTO;
 import ar.com.l_airline.entities.hotel.enums.Room;
 import ar.com.l_airline.services.HotelService;
 import ar.com.l_airline.ubications.City;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -18,8 +17,61 @@ import java.util.Optional;
 @RequestMapping("/hotel")
 public class HotelController {
 
-    @Autowired
-    private HotelService service;
+    private final HotelService service;
+
+    public HotelController(HotelService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/byId")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<Optional<Hotel>> getById(@RequestParam Long id){
+        Optional<Hotel> result = service.findHotelById(id);
+        if (result.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/byName")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<List<Hotel>> getByName(@RequestParam String name){
+        List<Hotel> result = service.findHotelByName(name);
+        if (result.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/byCity")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<List<Hotel>> findByCity(@RequestParam City city){
+        List<Hotel> result = service.findHotelByCity(city);
+        if (result.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/byRoom")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<List<Hotel>> findByRoom(@RequestParam Room room){
+        List<Hotel> result = service.findHotelByRoom(room);
+        if (result.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/byPrice")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<List<Hotel>> findByPrice(@RequestParam double min, @RequestParam double max){
+        List<Hotel> result = service.findHotelByPrice(min, max);
+        if (result.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
 
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @PostMapping("/insert")
@@ -32,71 +84,21 @@ public class HotelController {
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/byId/{id}")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public ResponseEntity<Optional<Hotel>> getById(@PathVariable Long id){
-        Optional<Hotel> result = service.findHotelById(id);
-        if (result.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(result);
-    }
-
-    @GetMapping("/byName/{name}")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public ResponseEntity<List<Hotel>> getByName(@PathVariable String name){
-        List<Hotel> result = service.findHotelByName(name);
-        if (result.isEmpty()){
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok(result);
-    }
-
-    @GetMapping("/byCity/{city}")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public ResponseEntity<List<Hotel>> findByCity(@PathVariable City city){
-        List<Hotel> result = service.findHotelByCity(city);
-        if (result.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(result);
-    }
-
-    @GetMapping("/byRoom/{room}")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public ResponseEntity<List<Hotel>> findByRoom(@PathVariable Room room){
-        List<Hotel> result = service.findHotelByRoom(room);
-        if (result.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(result);
-    }
-
-    @GetMapping("/byPrice/{min}/{max}")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public ResponseEntity<List<Hotel>> findByPrice(@PathVariable double min, @PathVariable double max){
-        List<Hotel> result = service.findHotelByPrice(min, max);
-        if (result.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(result);
-    }
-
-    @PatchMapping("/update/{id}")
+    @DeleteMapping("/delete")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Hotel> updateHotel (@PathVariable Long id, @RequestBody HotelDTO dto){
-        Hotel updatedHotel = service.updateHotel(id, dto);
-
-        return ResponseEntity.ok(updatedHotel);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<String> deleteHotel(@PathVariable Long id){
+    public ResponseEntity<String> deleteHotel(@RequestParam Long id){
         boolean result = service.deleteHotelById(id);
         if (!result){
             return ResponseEntity.ok("Operation can't be complete.");
         }
         return ResponseEntity.ok("¡Hotel deleted!");
+    }
+
+    @PatchMapping("/update")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Hotel> updateHotel (@RequestParam Long id, @RequestBody HotelDTO dto){
+        Hotel updatedHotel = service.updateHotel(id, dto);
+
+        return ResponseEntity.ok(updatedHotel);
     }
 }
