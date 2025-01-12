8cd6e0a79b740d9866b53cc8981d4ec02e943c45
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-01-12 18:26:15 -0300

    CHANGE: add @NoArgsConstructor in UserDTO

--- src/main/java/ar/com/l_airline/domains/dto/UserDTO.java
+++ src/main/java/ar/com/l_airline/domains/dto/UserDTO.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private Long id;
     private String email;
