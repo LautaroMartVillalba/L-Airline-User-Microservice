270ea0ff6dbbc935b4059a6811fc11697c54158b
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-01-14 18:33:45 -0300

    REFACTOR: changed json credentials implementation to use Vault

--- src/main/java/ar/com/l_airline/domains/email/EmailConfig.java
+++ src/main/java/ar/com/l_airline/domains/email/EmailConfig.java
@@ -1,31 +1,24 @@
 package ar.com.l_airline.domains.email;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
 import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
 
-    //Use ObjectMapper to get the secret JSON values.
-    ObjectMapper objectMapper = new ObjectMapper();
-    InputStream inputStream = new ClassPathResource("MailCredentials.json").getInputStream();
-    Map<String, String> jsonParsed = objectMapper.readValue(inputStream, Map.class);
-
     //Project's owner mail.
-    private final String email = jsonParsed.get("email.username");
+    @Value("${email.address}")
+    private String email;
     //Project's owner application password.
-    private final String password = jsonParsed.get("email.password");
+    @Value("${email.password}")
+    private String password;
 
     public EmailConfig() throws IOException {
     }
